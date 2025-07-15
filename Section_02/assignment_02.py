541fda8c03bef86178e44389a8fa2c4bffb0f67e
vidisi <35021846+vidisi@users.noreply.github.com>
2025-07-15 05:40:20 +0100

    Update assignment_02.py

--- Section_02/assignment_02.py
+++ Section_02/assignment_02.py
@@ -11,7 +11,7 @@ who owns a bike, a laptop and boat.
 
 # your code below:
 
-
+my_list = [{'Tom':{'salary':20000,'age':22,'owns':['jacket','car','TV']}},{'Mike':{'salary':24000,'age':27,'owns1':['bike','laptop','boat']}}]
 
 
 
